7a89771a7ffa6afa2408541ecd14795e4f7c55bc
timchiu1212 <911220timchiu@gmail.com>
2023-11-03 14:24:38 +0800

    LAB5

---
+++ app/src/main/java/com/example/lab5/costom_toast.java
@@ -0,0 +1,14 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class costom_toast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_costom_toast);
+    }
+}
